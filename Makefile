5663c1dc4366c8f84e3b5222d1becd624dbfc81a
Forest Gregg <fgregg@datamade.us>
2021-10-06 11:40:16 -0400

    included cities

--- Makefile
+++ Makefile
@@ -1,20 +1,30 @@
+geographies.geojson : cps.db
+	spatialite $< < scripts/cps_geography.sql
+
+	ogr2ogr -f GeoJSON $@ $< -sql 'select * from cps_geography where GEOMETRY IS NOT NULL' -dialect sqlite
+
+
 omnibus.geojson : cps.db
 	ogr2ogr -f GeoJSON $@ $< -sql @scripts/omnibus.sql -dialect sqlite
 
-cps.db : cps.csv cb_2018_us_county_20m.shp cb_2018_us_cbsa_20m.shp cb_2018_us_necta_500k.shp cb_2018_us_state_20m.shp
-	ogr2ogr -f SQLite -dsco SPATIALITE=YES $@ $(word 2,$^) -nlt PROMOTE_TO_MULTI
-	ogr2ogr -f SQLite -dsco SPATIALITE=YES -append $@ $(word 3,$^) -nlt PROMOTE_TO_MULTI
-	ogr2ogr -f SQLite -dsco SPATIALITE=YES -append $@ $(word 4,$^) -nlt PROMOTE_TO_MULTI
-	ogr2ogr -f SQLite -dsco SPATIALITE=YES -append $@ $(word 5,$^) -nlt PROMOTE_TO_MULTI
-	csvs-to-sqlite $< $@
+cps.db : cb_2018_us_county_20m.shp cb_2018_us_cbsa_20m.shp cb_2018_us_state_20m.shp us_principal_cities.shp cps.csv city_map.csv 
+	ogr2ogr -f SQLite -dsco SPATIALITE=YES -t_srs "EPSG:4326" $@ $(word 1,$^) -nlt PROMOTE_TO_MULTI
+	ogr2ogr -f SQLite -dsco SPATIALITE=YES -append -t_srs "EPSG:4326" $@ $(word 2,$^) -nlt PROMOTE_TO_MULTI
+	ogr2ogr -f SQLite -dsco SPATIALITE=YES -append -t_srs "EPSG:4326" $@ $(word 3,$^) -nlt PROMOTE_TO_MULTI
+	ogr2ogr -f SQLite -dsco SPATIALITE=YES -append -t_srs "EPSG:4326" $@ $(word 4,$^) -nlt PROMOTE_TO_MULTI
+	csvs-to-sqlite $(filter-out %.shp,$^) $@
+	spatialite $@ < scripts/cps_geography.sql
+
 
 cps.csv : cps_00006.csv.gz
 	gunzip -c $< > $@
 
-cb_%.shp : cb_%.zip
+%.shp : %.zip
 	unzip $<
+	touch $@
 
 cb_%.zip :
 	wget -O $@ "https://www2.census.gov/geo/tiger/GENZ2018/shp/$@"
 
-
+us_principal_cities.zip :
+	wget -O $@ "https://github.com/andrewvanleuven/website/raw/master/static/files/data/shp/us_principal_cities.zip"
--- scripts/omnibus.sql
+++ scripts/omnibus.sql
@@ -1,113 +1,14 @@
-WITH counties AS (
-    SELECT
-        geoid,
-        substr('00' || STATEFIP, -2, 2) AS state_geoid,
+SELECT geoid,
         GEOMETRY,
         COUNT(*) FILTER (WHERE EARNWT > 0.0) AS observations,
         SUM(EARNWT) / 60 AS employment,
         COALESCE(SUM(EARNWT / 60) FILTER (WHERE "UNION" = 2), 0) AS members,
         COALESCE(SUM(EARNWT / 60) FILTER (WHERE "UNION" > 1), 0) AS covered
-    FROM
-        cps
-        INNER JOIN cb_2018_us_county_20m ON geoid = substr('00000' || COUNTY, -5, 5)
-    WHERE
+from cps
+inner join cps_geography
+USING (METFIPS, STATEFIP, COUNTY, INDIVIDCC)
+   WHERE
         ELIGORG = 1
         AND YEAR >= 2016
         AND YEAR <= 2020
-    GROUP BY
-        STATEFIP,
-        COUNTY
-),
-remaining_metro AS (
-    SELECT
-        metro.geoid AS geoid,
-        CASE WHEN ST_UNION (counties.GEOMETRY) IS NOT NULL THEN
-            ST_DIFFERENCE (metro.GEOMETRY, ST_UNION (counties.GEOMETRY))
-        ELSE
-            metro.GEOMETRY
-        END AS GEOMETRY
-    FROM
-        cb_2018_us_cbsa_20m AS metro
-        LEFT JOIN counties ON St_Contains (metro.GEOMETRY, counties.GEOMETRY)
-    GROUP BY
-        metro.geoid,
-        metro.GEOMETRY
-),
-metros AS (
-    SELECT
-        remaining_metro.geoid,
-        state.geoid,
-        ST_CollectionExtract (ST_INTERSECTION (remaining_metro.GEOMETRY, state.GEOMETRY), 3) AS GEOMETRY,
-    COUNT(*) FILTER (WHERE EARNWT > 0.0) AS observations,
-    SUM(EARNWT) / 60 AS employment,
-    COALESCE(SUM(EARNWT / 60) FILTER (WHERE "UNION" = 2), 0) AS members,
-    COALESCE(SUM(EARNWT / 60) FILTER (WHERE "UNION" > 1), 0) AS covered
-FROM
-    cps
-    INNER JOIN remaining_metro ON remaining_metro.geoid = METFIPS
-    INNER JOIN cb_2018_us_state_20m AS state ON state.geoid = substr('00' || STATEFIP, -2, 2)
-    WHERE
-        ELIGORG = 1
-        AND YEAR >= 2016
-        AND YEAR <= 2020
-        AND COUNTY = 0
-    GROUP BY
-        METFIPS,
-        STATEFIP
-),
-subregion AS (
-    SELECT
-        *
-    FROM
-        metros
-    UNION
-    SELECT
-        *
-    FROM
-        counties
-),
-remaining_state AS (
-    SELECT
-        state.geoid AS geoid,
-        CASE WHEN ST_UNION (subregion.GEOMETRY) IS NOT NULL THEN
-            ST_DIFFERENCE (state.GEOMETRY, ST_UNION (subregion.GEOMETRY))
-        ELSE
-            state.GEOMETRY
-        END AS GEOMETRY
-    FROM
-        cb_2018_us_state_20m AS state
-        LEFT JOIN subregion ON St_Contains (state.GEOMETRY, subregion.GEOMETRY)
-    GROUP BY
-        state.geoid,
-        state.GEOMETRY
-),
-states AS (
-    SELECT
-        remaining_state.geoid,
-        remaining_state.geoid,
-        GEOMETRY,
-        COUNT(*) FILTER (WHERE EARNWT > 0.0) AS observations,
-        SUM(EARNWT) / 60 AS employment,
-        COALESCE(SUM(EARNWT / 60) FILTER (WHERE "UNION" = 2), 0) AS members,
-    COALESCE(SUM(EARNWT / 60) FILTER (WHERE "UNION" > 1), 0) AS covered
-FROM
-    cps
-    INNER JOIN remaining_state ON remaining_state.geoid = substr('00' || STATEFIP, -2, 2)
-    WHERE
-        ELIGORG = 1
-        AND YEAR >= 2016
-        AND YEAR <= 2020
-        AND COUNTY = 0
-        AND METFIPS = '99998'
-    GROUP BY
-        STATEFIP
-)
-SELECT
-    *
-FROM
-    subregion
-UNION
-SELECT
-    *
-FROM
-    states
+GROUP BY METFIPS, STATEFIP, COUNTY, INDIVIDCC;
